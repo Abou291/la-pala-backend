c0258fe4ba363846877827fd5d2dad496b218c60
Abou291 <mb6xnyzrvx@privaterelay.appleid.com>
2026-06-21 23:09:34 +0200

    fix render postgres ssl

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const { Pool } = require("pg");
+
 require("dotenv").config({ path: __dirname + "/.env" });
 
+console.log("DB_HOST =", process.env.DB_HOST);
 console.log("DB_USER =", process.env.DB_USER);
 console.log("DB_NAME =", process.env.DB_NAME);
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: Number(process.env.DB_PORT || 5432),
   user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
